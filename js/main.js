3f1b8da248ba26a4a9540db8e1c3b925b905d406
GalluFrancesco <latuamaildigithub@email.comfrancesco.gallu@gmail.com>
2022-01-11 15:43:52 +0100

    Add: array oggetti posts

---
+++ js/main.js
@@ -0,0 +1,33 @@
+
+
+//creo un array di post
+const posts = [
+
+    {
+        id: 1,
+        autore: "Tizio",
+        picAutore: "https://unsplash.it/300/300?image=100",
+        dataPost: "12-22-2020",
+        txtPost: "Testo scritto da Tizio",
+        imgPost:"https://unsplash.it/300/300?image=70",
+        counterLike: 70
+    },
+    {
+        id: 2,
+        autore: "Caio",
+        picAutore: "https://unsplash.it/300/300?image=200",
+        dataPost: "12-22-2020",
+        txtPost: "Testo scritto da Caio",
+        imgPost:"https://unsplash.it/300/300?image=80",
+        counterLike: 80
+    },
+    {
+        id: 3,
+        autore: "Sempronio",
+        picAutore: "https://unsplash.it/300/300?image=300",
+        dataPost: "12-22-2020",
+        txtPost: "Testo scritto da Sempronio",
+        imgPost:"https://unsplash.it/300/300?image=90",
+        counterLike: 90
+    }
+]
