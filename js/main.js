acd5520abc33629fac1af7e88843c1b765f83c36
GalluFrancesco <latuamaildigithub@email.comfrancesco.gallu@gmail.com>
2022-01-11 16:41:33 +0100

    fix: funzione output

--- js/main.js
+++ js/main.js
@@ -23,13 +23,13 @@ function output(container, listPost){
                                     <div class="post__footer">
                                         <div class="likes js-likes">
                                             <div class="likes__cta">
-                                                <a class="like-button  js-like-button" href="#" data-postid="1">
+                                                <a class="like-button  js-like-button" href="#" data-postid="${listPost[i].id}">
                                                     <i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i>
                                                     <span class="like-button__label">Mi Piace</span>
                                                 </a>
                                             </div>
                                             <div class="likes__counter">
-                                                Piace a <b id="like-counter-1" class="js-likes-counter">${listPost[i].counterLike}</b> persone
+                                                Piace a <b id="like-counter-${listPost[i].id}" class="js-likes-counter">${listPost[i].counterLike}</b> persone
                                             </div>
                                         </div> 
                                     </div>            
